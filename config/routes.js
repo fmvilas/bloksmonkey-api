e65336201f3c726b4847cb6d6f8318697ae2020e
Fran <fmvilas@gmail.com>
2015-02-10 19:20:23 +0100

    Add routes for projects

--- config/routes.js
+++ config/routes.js
@@ -2,30 +2,38 @@
 "use strict";
 
 module.exports = function(app, passport, oauth2server){
-	var express = require('express'),
-		routes = require('./route_table'),
-		api = express.Router();
+  var express = require('express'),
+    routes = require('./route_table'),
+    api = express.Router();
 
-	// Session routes
-	var session = require('../app/controllers/session')(routes, passport, oauth2server);
-	api.get(routes.session.login, session.login);
-	api.post(routes.session.login, session.logon);
-	api.get(routes.session.logout, session.logout);
+  // Session routes
+  var session = require('../app/controllers/session')(routes, passport, oauth2server);
+  api.get(routes.session.login, session.login);
+  api.post(routes.session.login, session.logon);
+  api.get(routes.session.logout, session.logout);
 
-	// User routes
-	var user = require('../app/controllers/user')(routes, passport, oauth2server);
-	api.get(routes.user.single, user.show);
-	api.post(routes.user.collection, user.create);
-	api.patch(routes.user.single, user.update);
+  // User routes
+  var user = require('../app/controllers/user')(routes, passport, oauth2server);
+  api.get(routes.user.single, user.show);
+  api.post(routes.user.collection, user.create);
+  api.patch(routes.user.single, user.update);
+
+  // Project routes
+  var project = require('../app/controllers/project')(routes, passport, oauth2server);
+  api.get(routes.project.collection, project.list);
+  api.get(routes.project.single, project.show);
+  api.post(routes.project.collection, project.create);
+  api.patch(routes.project.single, project.update);
+  api.delete(routes.project.single, project.remove);
 
 
-	// OAuth2 routes
-	var oauth2 = require('../app/controllers/oauth2')(routes, passport, oauth2server);
-	api.get(routes.oauth2.authorize, oauth2.authorize);
-	api.post(routes.oauth2.decision, oauth2.decision);
-	api.post(routes.oauth2.token, oauth2.token);
+  // OAuth2 routes
+  var oauth2 = require('../app/controllers/oauth2')(routes, passport, oauth2server);
+  api.get(routes.oauth2.authorize, oauth2.authorize);
+  api.post(routes.oauth2.decision, oauth2.decision);
+  api.post(routes.oauth2.token, oauth2.token);
 
-	app.use(routes.root, api);
+  app.use(routes.root, api);
 
-	return routes;
+  return routes;
 };
