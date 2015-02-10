2e3aa8a1326d2e2d18b49fdf1b7c441e3ca5d29c
Fran <fmvilas@gmail.com>
2015-02-10 21:19:58 +0100

    Modify user controller

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,106 +1,62 @@
 /*jslint vars:true, node:true */
 "use strict";
 
-var Static = {},
-	User = require('../models/user'),
-	json_tpl = require('templo'),
-	tpl;
+var UserService = require('../services/user/user'),
+    mongoose = require('mongoose'),
+    service = new UserService(mongoose.connection);
 
 
-Static.error403 = function(err, res) {
-	res.status(403).json({
-		status: 403,
-		message: err.message
-	});
-};
+function respondWithError(err, res) {
+  var res_info = {
+    status: err.status,
+    message: err.message
+  };
 
-Static.error404 = function(req, res) {
-	res.status(404).json({
-		status: 404,
-		message: "Can't find resource with id " + req.params.id + "."
-	});
-};
+  if( err.errors ) { res_info.errors = err.errors; }
+  if( err.warnings ) { res_info.warnings = err.warnings; }
 
-Static.error500 = function(err, res) {
-	res.status(500).json({
-		status: 500,
-		message: err.message
-	});
-};
+  res.status(err.status).json(res_info);
+}
+
 
 module.exports = function(routes, passport, oauth2server) {
 
-	/**
-	 * Sends the user.
-	 * GET /api/v1/users/:id
-	 */
-	var show = function (req, res, next) {
-		tpl = require('../templates/json/user');
+  /**
+   * Sends the user.
+   * GET /api/v1/users/:id
+   */
+  var show = function (req, res, next) {
+    try {
+      service.find(req.params, function(user) {
+        return res.json(user);
+      });
+    } catch(e) {
+      return respondWithError(e, res);
+    }
+  };
 
-		User.findById(req.params.id, function(err, user) {
-			if (err) { return Static.error500(err, res); }
+  /**
+   * Updates a user and sends it.
+   * PATCH /api/v1/users/:id
+   */
+  var update = function (req, res, next) {
+    try {
+      service.update(req.params, req.body, function(user) {
+        return res.json(user);
+      });
+    } catch(e) {
+      return respondWithError(e, res);
+    }
+  };
 
-			res.json(json_tpl.parse(tpl.show, user));
-		});
-	};
-
-	/**
-	 * Updates a user and sends it.
-	 * PATCH /api/v1/users/:id
-	 */
-	var update = function (req, res, next) {
-		tpl = require('../templates/json/user');
-
-		if( req.body.id || req.body.email ) {
-			return Static.error403(new Error('Forbidden. "id" and "email" are protected attributes.'), res);
-		}
-
-		User.findOneAndUpdate({ _id: req.params.id }, req.body, function(err, user) {
-			if (err) { return Static.error500(err, res); }
-			if (!user) { return Static.error404(req, res); }
-
-			res.json(json_tpl.parse(tpl.show, user));
-		});
-	};
-
-	/**
-	 * Creates a user and sends it.
-	 * POST /api/v1/users
-	 */
-	var create = function (req, res, next) {
-		var new_user;
-
-		tpl = require('../templates/json/user');
-
-		try {
-			new_user = json_tpl.parse(tpl.create, req.body);
-		} catch(e) {
-			return res.status(422).json({
-				status: 422,
-				message: e.message
-			});
-		}
-
-		User.create(new_user, function(err, user) {
-			if (err) { return Static.error500(err, res); }
-
-			res.json(json_tpl.parse(tpl.show, user));
-		});
-	};
-
-	return {
-		show: [
-			//passport.authenticate('bearer', { session: false }),
-			show
-		],
-		/* TODO: ATTENTION! Do not expose create user to the API */
-		create: [
-			//passport.authenticate('bearer', { session: false }),
-			create
-		],
-		update: [
-			//passport.authenticate('bearer', { session: false }),
-			update
-		]
-	};
+  return {
+    show: [
+      //passport.authenticate('bearer', { session: false }),
+      show
+    ],
+    update: [
+      //passport.authenticate('bearer', { session: false }),
+      update
+    ]
+  };
 };
