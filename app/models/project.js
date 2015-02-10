f5a3c8451967bbc06da1a03fc04a335152b117d0
Fran <fmvilas@gmail.com>
2015-02-10 19:16:19 +0100

    Add project model

--- app/models/project.js
+++ app/models/project.js
@@ -2,10 +2,13 @@ var mongoose = require('mongoose'),
 	Schema = mongoose.Schema;
 
 var ProjectSchema = new Schema({
-	owner: { type: String, required: true },
-	lastModificationDate: { type: Number, default: null },
-	team: { type: Array, default: [] },
-	repoURL: { type: String, default: null }
+	name: { type: String, required: true },
+	description: { type: String, default: '' },
+	visibility: { type: String, default: 'public' },
+	owner_id: { type: Schema.Types.ObjectId, required: true },
+	members: { type: [Schema.Types.ObjectId], default: [] },
+	created_at: { type: Date },
+	updated_at: { type: Date }
 });
 
 module.exports = mongoose.model('Project', ProjectSchema);
