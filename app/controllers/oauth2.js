dffc46cf7ea0292b98e514faad98d0cd8ce436eb
Fran <fmvilas@gmail.com>
2015-02-19 21:01:10 +0100

    Cleaning OAuth2 server and controller

--- app/controllers/oauth2.js
+++ app/controllers/oauth2.js
@@ -14,90 +14,91 @@ var passport = require('passport'),
     isObjectID = dbHelpers.isObjectID;
 
 module.exports = function(routes, passport, oauth2server) {
-	return {
-		authorize: [
-			ensureLoggedIn,
-			oauth2server.authorization(function (clientID, redirectURI, done) {
-				if (!isObjectID(clientID)) {
-					var err = new Error('Invalid parameter value: client_id.');
-					err.status = 400;
-					err.code = 'invalid_request';
+  return {
+    authorize: [
+      ensureLoggedIn,
+      oauth2server.authorization(function (clientID, redirectURI, done) {
+        if (!isObjectID(clientID)) {
+          var err = new Error('Invalid parameter value: client_id.');
+          err.status = 400;
+          err.code = 'invalid_request';
 
-					return done(err);
-				}
+          return done(err);
+        }
 
-				Client.findById(clientID, function (err, client) {
-					if (err) { return done(err); }
-					if (!client) { return done(null, false); }
-					if (client.redirect_uri !== redirectURI) { return done(null, false); }
+        Client.findById(clientID, function (err, client) {
+          if (err) { return done(err); }
+          if (!client) { return done(null, false); }
+          if (client.redirect_uri !== redirectURI) { return done(null, false); }
 
-					return done(null, client, redirectURI);
-				});
-			}),
-			function (req, res, next) {
-				User.findById(req.user.id, function (err, user) {
-					if( err ) { return next(err); }
+          return done(null, client, redirectURI);
+        });
+      }),
+      function (req, res, next) {
+        User.findById(req.user.id, function (err, user) {
+          if( err ) { return next(err); }
 
-					if( user.plugins.indexOf(req.oauth2.client._id) !== -1 ) {
-						AuthorizationCode.findOne(
-						{
-							user_id: user._id,
-							client_id: req.oauth2.client._id,
-							redirect_uri: req.oauth2.redirectURI
-						},
-						function(err, authCode) {
-							if( err ) { return next(err); }
+          if( user.plugins.indexOf(req.oauth2.client._id) !== -1 ) {
+            AuthorizationCode.findOne(
+            {
+              user_id: user._id,
+              client_id: req.oauth2.client._id,
+              redirect_uri: req.oauth2.redirectURI
+            },
+            function(err, authCode) {
+              if( err ) { return next(err); }
+              if( !authCode ) { return next(false); }
 
-							var url = require('url'),
-								parsed = url.parse(req.oauth2.redirectURI, true);
+              var url = require('url'),
+                  parsed = url.parse(req.oauth2.redirectURI, true),
+                  location;
 
-							delete parsed.search;
-							parsed.query.code = authCode.code;
-							//if (txn.req && txn.req.state) { parsed.query.state = txn.req.state; }
+              delete parsed.search;
+              parsed.query.code = authCode.code;
+              location = url.format(parsed);
 
-							var location = url.format(parsed);
-
-							res.redirect(location);
-						});
-					} else {
-						res.render('user/allowplugin', {
-							transactionID: req.oauth2.transactionID,
-							plugin: req.oauth2.client
-						});
-					}
-				});
-			},
-			oauth2server.errorHandler()
-		],
-
-
-		decision: [
-			ensureLoggedIn,
-			oauth2server.decision(function(req, done) {
-				if( !req.body.cancel ) {
-					User.findByIdAndUpdate(
-					    req.user.id,
-					    {
-					    	$addToSet: {
-					    		plugins: req.oauth2.req.clientID
-					    	}
-					    },
-					    function (err, user) {
-					    	return done(err, {scope: req.body.scope});
-					    }
-					);
-				} else {
-					done();
-				}
-			}),
-			oauth2server.errorHandler()
-		],
+              res.redirect(location);
+            });
+          } else {
+            res.render('user/allowplugin', {
+              transactionID: req.oauth2.transactionID,
+              plugin: req.oauth2.client
+            });
+          }
+        });
+      },
+      oauth2server.errorHandler()
+    ],
 
 
-		token: [
-			passport.authenticate(['basic', 'oauth2-client-password'], { session: false }),
-			oauth2server.token(),
-			oauth2server.errorHandler()
-		]
-	};
+    decision: [
+      ensureLoggedIn,
+      authHelpers.validateScopes,
+      oauth2server.decision(function(req, done) {
+        if( !req.body.cancel ) {
+          User.findByIdAndUpdate(
+              req.user.id,
+              {
+                $addToSet: {
+                  plugins: req.oauth2.req.clientID
+                }
+              },
+              function (err, user) {
+                return done(err, {scope: req.body.scope});
+              }
+          );
+        } else {
+          done();
+        }
+      }),
+      oauth2server.errorHandler()
+    ],
+
+
+    token: [
+      passport.authenticate(['basic', 'oauth2-client-password'], { session: false }),
+      oauth2server.token(),
+      oauth2server.errorHandler()
+    ]
+  };
 };
--- config/oauth2server.js
+++ config/oauth2server.js
@@ -6,67 +6,74 @@ var oauth2orize = require('oauth2orize'),
     mongoose = require('mongoose'),
     Client = mongoose.model('Client'),
     AccessToken = mongoose.model('AccessToken'),
-    AuthorizationCode = mongoose.model('AuthorizationCode');
+    AuthorizationCode = mongoose.model('AuthorizationCode'),
+    moment = require('moment');
 
 var oauth2server = oauth2orize.createServer();
 
 oauth2server.serializeClient(function (client, done) {
-	return done(null, client._id);
+  return done(null, client._id);
 });
 
 oauth2server.deserializeClient(function (_id, done) {
-	Client.findById(_id, function (err, client) {
-		if (err) { return done(err); }
+  Client.findById(_id, function (err, client) {
+    if (err) { return done(err); }
 
-		return done(null, client);
-	});
+    return done(null, client);
+  });
 });
 
 oauth2server.grant(oauth2orize.grant.code(function (client, redirectURI, user, ares, done) {
-	var now = new Date().getTime(),
-	    code = crypto.createHmac('sha1', 'access_token')
-	           .update([client.id, now].join())
-	           .digest('hex');
+  var now = new Date().getTime(),
+      code = crypto.createHmac('sha1', 'access_token')
+             .update([client.id, now].join())
+             .digest('hex');
 
-	var ac = new AuthorizationCode({
-		code: code,
-		client_id: client.id,
-		redirect_uri: redirectURI,
-		user_id: client.user_id,
-		scope: ares.scope
-	});
+  AuthorizationCode.findOneAndUpdate({
+    user_id: user.id,
+    client_id: client.id
+  }, {
+    code: code,
+    redirect_uri: redirectURI,
+    scope: ares.scope
+  }, {
+    upsert: true
+  },
+  function(err, authorization_code) {
+    if( err ) { return done(err); }
 
-	ac.save(function (err) {
-		if (err) { return done(err); }
-		return done(null, code, {scope: ares.scope});
-	});
+    return done(null, authorization_code.code, { scope: authorization_code.scope });
+  });
 }));
 
 oauth2server.exchange(oauth2orize.exchange.code(function (client, code, redirectURI, done) {
-	AuthorizationCode.findOne({code: code}, function (err, code) {
-		if (err) { return done(err); }
-		if (!code || !code.client_id || !client || !client.id) { return done(null, false); }
-		if (client.id.toString() !== code.client_id.toString()) { return done(null, false); }
-		if (redirectURI !== code.redirect_uri) { return done(null, false); }
+  AuthorizationCode.findOneAndRemove({ code: code }, function (err, code) {
+    if( err ) { return done(err); }
+    if( !code || !code.client_id || !client || !client.id ) { return done(null, false); }
+    if( client.id.toString() !== code.client_id.toString() ) { return done(null, false); }
+    if( redirectURI !== code.redirect_uri ) { return done(null, false); }
 
-		var now = new Date().getTime(),
-		    token = crypto.createHmac('sha1', 'access_token')
-		    .update([client.id, now].join())
-		    .digest('hex');
+    var now = new Date().getTime(),
+        token_hash = crypto.createHmac('sha1', 'access_token')
+                      .update([client.id, now].join())
+                      .digest('hex');
 
+    AccessToken.findOneAndUpdate({
+      user_id: code.user_id,
+      client_id: client._id
+    }, {
+      oauth_token: token_hash,
+      expires: moment().add(30, 'minutes').toDate(),
+      scope: code.scope
+    }, {
+      upsert: true
+    },
+    function(err, access_token) {
+      if( err ) { return done(err); }
 
-		var at = new AccessToken({
-			oauth_token: token,
-			user_id: code.user_id,
-			client_id: client._id,
-			scope: code.scope
-		});
-
-		at.save(function (err) {
-			if (err) { return done(err); }
-			return done(null, token, {scope: code.scope});
-		});
-	});
+      return done(null, access_token.oauth_token, { scope: access_token.scope });
+    });
+  });
 }));
 
 module.exports = oauth2server;
