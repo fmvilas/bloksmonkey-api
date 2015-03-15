de172a31e83ce21df8e4ab8415f8fbcd97c715ba
Fran <fmvilas@gmail.com>
2015-03-15 17:36:24 +0100

    Add middleware to check scope

--- app/helpers/auth.js
+++ app/helpers/auth.js
@@ -34,5 +34,26 @@ module.exports = {
     }
 
     return next();
+  },
+
+  checkHasScope: function(scopes) {
+    return function(req, res, next) {
+      var regex,
+          has_scope = false;
+
+      for(var i = 0; i < scopes.length && !has_scope; i++) {
+        regex = new RegExp('(^'+scopes[i]+'[\\s$])|(\\s'+scopes[i]+'\\s)|(\\s'+scopes[i]+'$)');
+        has_scope = req.authInfo && req.authInfo.scope && req.authInfo.scope.match(regex);
+      }
+
+      if( !has_scope ) {
+        var err = new Error('Missing required scope/s <'+scopes.join(', ')+'>.');
+        err.status = 403;
+        err.code = 'forbidden_request';
+        return next(err);
+      }
+
+      return next();
+    };
   }
 };
