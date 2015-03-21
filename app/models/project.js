bee309c64ccc7fdafaa847444fd14fab8e2c2663
Fran <fmvilas@gmail.com>
2015-03-21 22:52:29 +0100

    Add methods to model

--- app/models/project.js
+++ app/models/project.js
@@ -11,4 +11,25 @@ var ProjectSchema = new Schema({
   updated_at: { type: Date, default: Date.now }
 });
 
+/**
+ * Checks if project owner id is the specified one.
+ *
+ * @param [ObjectId|String] user_id
+ * @returns [Boolean] Wether project owner id is the specified one or not.
+ */
+ProjectSchema.methods.has_owner = function(user_id) {
+  user_id = user_id+'';
+  return this.owner_id.toString() === user_id;
+};
+
+/**
+ * Checks if the user with the provided id is either a member or the owner.
+ *
+ * @param [ObjectId|String] user_id
+ * @returns [Boolean] Wether the user with the provided id is a member or owner.
+ */
+ProjectSchema.methods.has_member = function(user_id) {
+  return this.has_owner(user_id) && this.members.indexOf(user_id) === -1;
+};
+
 module.exports = mongoose.model('Project', ProjectSchema);
